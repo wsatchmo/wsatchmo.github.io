c6986823528a4ee3279f6411da0ade39c82c4e33
wsatchmo <wsatcher@outlook.com>
2020-04-10 18:04:42 -0700

    Added link buttons

---
+++ assets/js/buttons.js
@@ -0,0 +1,70 @@
+var linkEl = document.querySelector('.linkpath');
+var current = null;
+$('.github').hover(function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: linkEl,
+    strokeDashoffset: {
+      value: 0,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '240 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
+  });
+});
+$('.artstation').hover(function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: linkEl,
+    strokeDashoffset: {
+      value: -336,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '240 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
+  });
+});
+
+/* HAS EFFECT ON SPHERE
+var current = null;
+$('.github').hover(function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: 0,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '240 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
+  });
+});
+$('.artstation').hover(function(e) {
+  if (current) current.pause();
+  current = anime({
+    targets: 'path',
+    strokeDashoffset: {
+      value: -336,
+      duration: 700,
+      easing: 'easeOutQuart'
+    },
+    strokeDasharray: {
+      value: '240 1386',
+      duration: 700,
+      easing: 'easeOutQuart'
+    }
+  });
+});
+*/
